aa9fff668d6ce17d33c6ceb10ad569003a643e7a
oliviermplante <oliviermplante@gmail.com>
2023-03-06 19:38:58 -0500

    linkedin

--- vite-project/src/components/SocialMediaIcons.jsx
+++ vite-project/src/components/SocialMediaIcons.jsx
@@ -3,37 +3,13 @@ const SocialMediaIcons = () => {
     <div className="flex justify-center md:justify-start my-10 gap-7">
       <a
         className="hover:opacity-50 transition duration-500"
-        href="https://i1.sndcdn.com/artworks-000218598405-cflfsk-t500x500.jpg"
+        href="https://www.linkedin.com/in/olivier-meunier-plante-a23b56268/"
         target="_blank"
         rel="noreferrer"
       >
         <img alt="linkedin-link" src="../assets/linkedin.png" />
       </a>
-      <a
-        className="hover:opacity-50 transition duration-500"
-        href="https://img.freepik.com/premium-photo/close-up-face-beautiful-cassowary-bird-have-vivid-blue-color-beak-look-like-smile_43561-10.jpg?w=2000"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img alt="twitter-link" src="../assets/twitter.png" />
-      </a>
-      <a
-        className="hover:opacity-50 transition duration-500"
-        href="https://pbs.twimg.com/media/De7_fWFU0AAuI0E.jpg"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img alt="facebook-link" src="../assets/facebook.png" />
-      </a>
-      <a
-        className="hover:opacity-50 transition duration-500"
-        href="https://media.licdn.com/dms/image/C5608AQFpHv17JvyYFw/croft-frontend-shrinkToFit1024/0/1520458317143?e=2147483647&v=beta&t=Y9dRr6p0aTCfdRamI0E6mVqFEJulomZm8ttY0EOjZUM"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img alt="instagram-link" src="../assets/instagram.png" />
-      </a>
-    </div>
+    </div> 
   );
 };
 
